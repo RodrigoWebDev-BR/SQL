648849855725ad968540fc803b12a9b0ac260f9b
RodrigoWebDev-BR <cruz213.rodrigo@gmail.com>
2020-08-07 11:17:16 -0300

    first commit

---
+++ Dump-APP_MUSIC-DDL/app_music_artist_genre.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `app_music` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `app_music`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: app_music
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `artist_genre`
+--
+
+DROP TABLE IF EXISTS `artist_genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `artist_genre` (
+  `ARTIST_ID` int NOT NULL,
+  `GENRE_ID` int NOT NULL,
+  KEY `ARTIST_ID` (`ARTIST_ID`),
+  KEY `GENRE_ID` (`GENRE_ID`),
+  CONSTRAINT `artist_genre_ibfk_1` FOREIGN KEY (`ARTIST_ID`) REFERENCES `artists` (`ID`),
+  CONSTRAINT `artist_genre_ibfk_2` FOREIGN KEY (`GENRE_ID`) REFERENCES `genres` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artist_genre`
+--
+
+LOCK TABLES `artist_genre` WRITE;
+/*!40000 ALTER TABLE `artist_genre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `artist_genre` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-25 18:22:04
---
+++ UnderWorld-DML.sql
@@ -0,0 +1,162 @@
+INSERT INTO underworld.ARTIST (Id, ArtistName, ArtistPhoto) VALUES (DEFAULT, "Arch Enemy", "");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 1, "Wages of Sin", "", 2001);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 1, "Burning Angel", "", "00:04:17");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 1, "Ravenous", "", "00:04:06");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 1, "Savage Messiah", "", "00:05:18");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 1, "Khaos Legions", "", 2011);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 2, "Yesterday is Dead and Gone", "", "00:04:21");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 2, "No Gods, No Masters", "", "00:04:14");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 1, "Will to Power", "", 2017);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 3, "The Race", "", "00:03:15");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 3, "The World is Words", "", "00:04:53");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 1, 3, "Reason to Believe", "", "00:04:47");
+    
+INSERT INTO underworld.ARTIST (Id, ArtistName, ArtistPhoto) VALUES (DEFAULT, "Avenged Sevenfold", "");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 2, "City of Evil", "", 2005);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 4, "Seize the Day", "", "00:05:37");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 4, "Beast and the Harlot", "", "00:05:17");
+    
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 2, "Avenged Sevenfold", "", 2007);
+    
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 5, "Critical Acclaim", "", "00:05:18");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 5, "Gunslinger", "", "00:04:15");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 2, "Nightmare", "", 2010);
+    
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 6, "Nightmare", "", "00:06:20");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 6, "Buried Alive", "", "00:06:45");
+    
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 2, "Hailto the King", "", 2013);
+    
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 7, "Shepherd Of Fire", "", "00:05:22");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 7, "Hail to the King", "", "00:05:06");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 2, 7, "This Means War", "", "00:06:13");
+
+INSERT INTO underworld.ARTIST (Id, ArtistName, ArtistPhoto) VALUES (DEFAULT, "Bullet for my Valentine", "");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 3, "The Poison", "", 2005);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 8, "Tears Don't Fall", "", "00:05:48");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 8, "All These Things I Hate (Reveolve Around Me)", "", "00:03:45");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 8, "Cries in Vain", "", "00:03:56");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 3, "Scream Aim Fire", "", 2008);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 9, "Scream Aim Fire", "", "00:04:26");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 9, "Hearts Burts Into Fire", "", "00:04:58");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 9, "Road to Nowhere", "", "00:04:19");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 3, "Venom", "", 2015);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 10, "Worthless", "", "00:03:16");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 3, "Gravity", "", 2018);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 11, "Leap of Faith", "", "00:03:19");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 11, "Letting You Go", "", "00:03:43");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 3, 11, "Not Dead Yet", "", "00:03:21");
+
+INSERT INTO underworld.ARTIST (Id, ArtistName, ArtistPhoto) VALUES (DEFAULT, "Death", "");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 4, "Symbolic", "", 1995);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 4, 12, "Zero Tolerance", "", "00:04:48");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 4, 12, "Empty Words", "", "00:06:22");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 4, 12, "Without Judgement", "", "00:05:28");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released)
+	VALUES (DEFAULT, 4, "The Sound of Perseverance", "", 1998);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 4, 13, "", "", "00:06:44");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, Songtrack, Duration)
+	VALUES (DEFAULT, 4, 13, "", "", "00:08:25");
+    
+INSERT INTO underworld.ARTIST (Id, ArtistName, ArtistPhoto) VALUES (DEFAULT, "Iced Earth", "");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 5, "Nigth of the Stormrider", "", 1992);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 14, "Angels Holocaust", "", "00:04:52");
+
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 5, "Burnt Offerings", "", 1995);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 15, "Dante's Inferno", "", "00:16:29");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 5, "The Dark Saga", "", 1996);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 16, "Dark Saga", "", "00:03:43");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 16, "A Question of Heaven", "", "00:07:40");
+
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 5, "Something Wicked This Way Comes", "", 1998);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 17, "Burning Times", "", "00:03:42");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 17, "Melanholy (Holy Martir)", "", "00:04:44");
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 17, "Prophecy", "", "00:06:18");
+    
+INSERT INTO underworld.ALBUM (Id, ArtistId, AlbumTitle, AlbumPhoto, Released) 
+	VALUES (DEFAULT, 5, "Horror Show", "", 2001);
+
+INSERT INTO underworld.SONG (Id, ArtistId, AlbumId, SongTitle, SongTrack, Duration)
+	VALUES (DEFAULT, 5, 18, "Dracula", "", "00:05:51");
